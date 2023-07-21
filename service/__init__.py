0692a576e0320d59257d61b6cd3d50031ec5efc8
Dheison Gomes <dheisomgomes0@gmail.com>
2023-07-21 19:47:03 -0300

    feat: add dotenv support

--- service/__init__.py
+++ service/__init__.py
@@ -1,5 +1,8 @@
 from os import environ
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 DATABASE_URI = environ.get("DATABASE_URI", "")
 DESCRIPTION = "Postagens mais recentes de seus perfis preferidos!"
