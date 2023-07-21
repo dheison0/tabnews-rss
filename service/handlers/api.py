3ba54f52a119db3484037065df4e8ad74a2a5a61
Dheison Gomes <dheisomgomes0@gmail.com>
2023-07-21 20:54:33 -0300

    feat: add delete user handler

--- service/handlers/api.py
+++ service/handlers/api.py
@@ -20,6 +20,7 @@ async def add_user(request: Request):
         params={'page': 1, 'per_page': 1}
     )
     await session.close()
+    del session
     if response.status != HTTPStatus.OK:
         return json({'error': 'user not found'}, status=HTTPStatus.BAD_REQUEST)
     db.add_user(username)
@@ -29,4 +30,15 @@ async def add_user(request: Request):
 async def get_users(request: Request):
     users = [asdict(u) for u in db.get_users()]
     return json({'message': 'ok', 'users': users})
+
+
+async def remove_user(request: Request):
+    user = request.args.get('user')
+    if not user:
+        return json(
+            {'error': 'query "user" is missing'},
+            status=HTTPStatus.BAD_REQUEST
+        )
+    db.remove_user(user)
+    return json({'message': 'ok'})
 
--- service/handlers/register.py
+++ service/handlers/register.py
@@ -5,5 +5,6 @@ from sanic import Sanic
 def add_handlers(server: Sanic):
     server.add_route(api.add_user, '/', ['POST'])
     server.add_route(api.get_users, '/', ['GET'])
+    server.add_route(api.remove_user, '/', ['DELETE'])
     server.add_route(rss.rss_feed, '/rss', ['GET'])
 
