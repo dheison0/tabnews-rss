dd1c4025dea0d54d4e91782713863e60ddf54a7e
Dheison Gomes <dheisomgomes0@gmail.com>
2023-07-29 18:50:22 -0300

    Add support to set database file and listen port by environment variable

--- tabrss.go
+++ tabrss.go
@@ -4,20 +4,30 @@ import (
 	"tabrss/service/database"
 	"tabrss/service/handler"
 
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	dbFile := os.Getenv("DATABASE_FILE")
+	if port == "" {
+		port = "8080"
+	}
+	if dbFile == "" {
+		dbFile = "tablocal.db"
+	}
 	server := gin.Default()
 	db := &database.SQLITEDatabase{}
-	if err := db.Open("tablocal.db"); err != nil {
+	if err := db.Open(dbFile); err != nil {
 		panic(err)
 	}
 	defer db.Close()
 	handler.AddHandlers(server, db)
 	server.StaticFile("/", "web/index.html")
 	server.Static("/web", "web")
-	if err := server.Run(":8080"); err != nil {
+	if err := server.Run(":" + port); err != nil {
 		panic(err)
 	}
 }
